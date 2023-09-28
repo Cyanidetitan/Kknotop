ee39d48182101160c053729d8eeab9467c0959fc
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-09-28 16:04:42 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,3 @@
-token = "token here" # As string
-group_id = group_id_here # As int including minus sign if any.   
+token = "6535153154:AAGuymX2nG6wrkfxVxe6Bqmqef2QMJRN0Cs" # As string
+group_id = -1001975432025 # As int including minus sign if any.   
 
